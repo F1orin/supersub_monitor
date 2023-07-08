606d47ea3d93a91c262f7ec734a7b728f3c6ee6c
Florin Bicher <florin.bicher@xolo.io>
2023-07-08 17:03:07 +0200

    fix deploy script

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@
 cd /opt/supersub_monitor
 logger "Pulling new version"
 sudo git pull
-HEAD=git rev-parse --short HEAD
+HEAD=$(git rev-parse --short HEAD)
 logger "Launching commit: $HEAD"
 sudo systemctl restart supersub
 logger "Service restarted"
