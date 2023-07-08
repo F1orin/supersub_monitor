47bbf2458c6fb23f291259c01181e9f7de7c49af
Florin Bicher <florin.bicher@xolo.io>
2023-07-08 16:58:16 +0200

    add logging to deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
 cd /opt/supersub_monitor
+logger "Pulling new version"
 sudo git pull
+HEAD=git rev-parse --short HEAD
+logger "Launching commit: $HEAD"
 sudo systemctl restart supersub
+logger "Service restarted"
