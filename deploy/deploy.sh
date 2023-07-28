1435fb12a92d2aed370fc5b1966e1e21ff1802a4
Florin Bicher <florin.bicher@xolo.io>
2023-07-28 14:40:25 +0200

    fix getting commit hash in deploy

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -7,7 +7,7 @@ sudo git pull
 logger "Installing requirements"
 sudo .venv/bin/pip install -r requirements.txt
 
-HEAD=$(git rev-parse --short HEAD)
+HEAD=$(sudo git rev-parse --short HEAD)
 logger "Launching commit: $HEAD"
 sudo systemctl restart supersub
 
