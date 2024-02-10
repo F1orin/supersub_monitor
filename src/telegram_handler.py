417608ea788ea7a45009e67127ee0251150d764a
Florin Bicher <f.bicher@gmail.com>
2024-02-10 01:08:39 +0100

    Message every 5s works

--- src/telegram_handler.py
+++ src/telegram_handler.py
@@ -18,10 +18,13 @@ def prepare_message(city: str, matches: list) -> str:
 
 async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     """Send a message when the command /help is issued."""
-    await update.message.reply_text('Type /check to search for matches.')
+    help_message = 'Type /check to search for matches.\n\n' \
+        + 'Type /subscribe to receive updates every day.\n\n' \
+        + 'Type /unsubscribe to stop receiving regular updates.\n\n'
+    await update.message.reply_text(help_message)
 
 
-async def check_command(update: Update, context: ContextTypes.DEFAULT_TYPE, city: str) -> None:
+async def check_command(city: str, update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     """Check for available Supersub matches when the command /check is issued."""
     telegram_message = await update.message.reply_text('Checking available matches...')
     try:
@@ -36,6 +39,34 @@ async def check_command(update: Update, context: ContextTypes.DEFAULT_TYPE, city
         await telegram_message.edit_text(f'Selenium WebDriverException occured')
 
 
+async def send_message(context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Send the message."""
+    job = context.job
+    await context.bot.send_message(job.chat_id, text='qwe')
+
+
+def remove_jobs(context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Remove all jobs from the queue."""
+    current_jobs = context.job_queue.jobs()
+    for job in current_jobs:
+        job.schedule_removal()
+
+
+async def start_recurring_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Start regular checks for SuperSub matches."""
+    chat_id = update.effective_message.chat_id
+    context.job_queue.run_repeating(send_message, interval=5, chat_id=chat_id)
+    text = 'Recurring updates started'
+    await update.message.reply_text(text)
+
+
+async def stop_recurring_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Stop regular checks for SuperSub matches."""
+    remove_jobs(context)
+    text = 'Recurring updates stopped'
+    await update.message.reply_text(text)
+
+
 def start_telegram_bot(token, city):
     application = Application.builder().token(token).build()
 
@@ -43,5 +74,9 @@ def start_telegram_bot(token, city):
 
     application.add_handler(CommandHandler("help", help_command))
     application.add_handler(CommandHandler("check", check_command_with_city))
+    application.add_handler(CommandHandler(
+        "subscribe", start_recurring_command))
+    application.add_handler(CommandHandler(
+        "unsubscribe", stop_recurring_command))
 
     application.run_polling()
