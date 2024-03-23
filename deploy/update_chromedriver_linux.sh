407c0f4136fbf36e880ba235c7757af0afd33100
Florin Bicher <f.bicher@gmail.com>
2024-03-23 21:45:58 +0100

    improve chromedriver updating script

--- deploy/update_chromedriver_linux.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-CHROMEDRIVER_URL="https://edgedl.me.gvt1.com/edgedl/chrome/chrome-for-testing/121.0.6167.85/linux64/chromedriver-linux64.zip"
-
-wget "$CHROMEDRIVER_URL" -P /tmp/chromedriver
-cd /tmp/chromedriver/ || exit
-unzip chromedriver-linux64.zip
-mv /tmp/chromedriver/chromedriver-linux64/chromedriver /opt/supersub_monitor/drivers/chromedriver_linux
